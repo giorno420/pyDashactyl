5aa92b7b9a1cb47122a60927e11289d6667f8b57
giorno420 <74859235+giorno420@users.noreply.github.com>
2021-07-22 13:32:41 +0100

    changed directory structure

--- api/redeem.py
+++
@@ -1,6 +0,0 @@
-from pydactyl import PterodactylClient
-import configfile
-import paypalrestsdk
-from paypalrestsdk import Payment, Order
-
-client = PterodactylClient(configfile.pteroURL, configfile.pteroAppKey)
